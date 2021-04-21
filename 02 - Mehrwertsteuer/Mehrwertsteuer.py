d43ef0097075d8d067de577262e479ed89cd47a7
Michael Beurskens <beursken@uni-duesseldorf.de>
2021-04-21 23:54:20 +0200

    Initial Checkin

---
+++ 02 - Mehrwertsteuer/Mehrwertsteuer.py
@@ -0,0 +1,22 @@
+# Programm 01: Kostenquote nach §§ 91, 92 ZPO im Zweipersonenverhältnis
+# (C) 13.04.2021, Michael Beurskens
+
+
+print("Dieses Programm berechnet Preise mit und ohne Umsatzsteuer (Brutto und Nettopreise)")
+betragText: str = input("Betrag (in Euro): ")
+steuersatzText: str = input("Steuersatz (in Prozent): ")
+
+betrag: float = round(float(betragText),2)
+steuersatz: float = round(float(steuersatzText),2)
+brutto: float = round(betrag*(100 + steuersatz)/100,2)
+netto: float = round(betrag / ((100 + steuersatz)/100),2)
+steuerBrutto:float = round(brutto-betrag,2)
+steuerNetto :float = round(betrag-netto,2)
+print("\n")
+print("-----------------------------------------------------------------------")
+
+print(f"{betrag:,.2f} € netto + {steuersatz}% \
+Umsatzsteuer ({steuerBrutto:,.2f} €) = {brutto:,.2f} € brutto.")
+print(f"{betrag:,.2f} € brutto = {netto:,.2f} € netto + {steuersatz}% \
+Umsatzsteuer ({steuerNetto:,.2f} €).")
+print("-----------------------------------------------------------------------")
---
+++ 03 - Blutalkohol/Blutalkohol.py
@@ -0,0 +1,26 @@
+# Programm 03: Blutalkoholkonzentration nach der Widmark-Formel
+# (C) 20.04.2021, Michael Beurskens
+
+alkoholDichte:float=0.8
+print("Dieses Programm berechnet die Alkoholkonzentration im Blut nach der sog. Widmark-Formel.")
+print("\n")
+volumenText:str=input("Wie viel Milliliter haben Sie getrunken? ")
+alkoholText:str=input("Wie viel Alkohol enthielt das Getränk in Prozent? ")
+gewichtText:str=input("Wie viel wiegen Sie in Kilogramm? ")
+resorptionText:str=input("Geben Sie jetzt den sog. Verteilungsfaktor ein - \
+dieser beträgt für Männer 0.7, für Frauen 0.6 oder für Kinder 0.8: ")
+
+volumen:float=float(volumenText)
+alkohol:float=float(alkoholText)/100
+gewicht:float=float(gewichtText)
+resorption:float=float(resorptionText)
+alkoholMasse:float=volumen*alkohol*alkoholDichte
+blutalkohol:float=alkoholMasse/(gewicht*resorption)
+print("\n")
+print("---------------------------------------------------")
+print("Masse aufgenommenen Alkohols = Getränkemenge in Milliliter * Alkoholvolum in Prozent * Dichte von Alkohol")
+print("<=> %.2f g = %.2f ml * %.2f %% * %.2f g/ml"%(alkoholMasse,volumen,alkohol*100,alkoholDichte))
+print("\n")
+print("Promille im Körper = Masse aufgenommenen Alkohols / (Gewicht * Verteilungsfaktor)")
+print("<=> %.2f ‰ = %.2f g / (%.2f kg * %.2f)"%(blutalkohol,alkoholMasse,gewicht,resorption))
+print("---------------------------------------------------")
