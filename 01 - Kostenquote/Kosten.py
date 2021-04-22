a785609ac32f65b15cc0405a44130511a21f1de6
Michael Beurskens <beursken@uni-duesseldorf.de>
2021-04-22 23:53:09 +0200

    Mehr erklärende Kommentare beim Kostenprogramm

--- 01 - Kostenquote/Kosten.py
+++ 01 - Kostenquote/Kosten.py
@@ -1,25 +1,41 @@
 # Programm 01: Kostenquote nach §§ 91, 92 ZPO im Zweipersonenverhältnis
 # (C) 13.04.2021, Michael Beurskens
 
+# Im Folgenden werden einige Variablen definiert, die wir später brauchen. Dies geschieht durch Angabe des Namens, 
+# gefolgt von einem Doppelpunkt mit dem Typ und einem Gleichheitszeichen mit dem Wert
+
 streitwert: float = 0
 erfolg: float = 0
 unterliegen: float = 0
 kostenKlaeger: float = 0
 kostenBeklagter: float = 0
 
+
+# Mit der Funktion print() kann man Texte ausgeben. Diese stehen in Anführungszeichen.
 print("Dieses Programm berechnet die Kostenquote nach §§ 91, 92 ZPO im Zweipersonenverhältnis")
+
+# Mit der Funktion input() kann man Texte, die über die Tastatur eingegeben werden, in eine Variable speichern. Dazu schreibt man einfach Variablenname : str = input().
 eingabeStreitwert: str = input("Wie hoch ist der Streitwert (in Euro)? ")
 eingabeErfolg: str = input(
     "In welchem Umfang war die Klage erfolgreich (in Euro)? ")
 
+# Man kann Werte umwandeln (etwa einen Text in eine Zahl), indem man den gewünschten Typ vor eine Variable setzt und diese in Klammern dahinterschreibt, also z.B. int(jahr), str(zahl)
 streitwert = float(eingabeStreitwert)
 erfolg = float(eingabeErfolg)
 unterliegen = streitwert-erfolg
 kostenKlaeger = round((streitwert-erfolg)/streitwert*100, 2)
 kostenBeklagter = 100-kostenKlaeger
 
+# Wenn man \n im Rahmen eines print()-Befehls eingibt, wird eine leere Zeile ("Absatz") ausgegeben
 print("\n")
 print("-----------------------------------------------------------------------")
+
+# Hier folgt jetzt eine etwas kompliziertere Ausgabe. Dabei ist folgendes zu beachten:
+# 1. Wenn eine Zeile zu lang wird, kann man diese mit einem Backslash (Alt Gr+ ß) in der nächsten Zeile fortsetzen.
+# 2. Wenn man ein Prozentzeichen in einem Text angibt, wird dieses durch Wert ersetzt, die man nach dem Text auflistet.
+# 3. Wenn man hinter dem Prozentzeichen %f angibt, weiß der Computer, dass eine Gleitkommazahl gewünscht ist.
+# 4. Wenn man hinter dem Prozentzeichen %.2f angibt, weiß der Computer, dass hinter dem Punkt (Komma) nur zwei Ziffern stehen sollen (der Rest fällt ohne Rundung weg)
+# 5. Wenn es in einem Text mehrere Lücken gibt, werden die einzufügenden Werte in einer Klammer hinter dem Text angegeben
 print("Die Kosten des Rechtsstreits trägt der Kläger zu %.2f / %.2f \
 und der Beklagte zu %.2f / %.2f." % (unterliegen, streitwert, erfolg, streitwert))
 print("\n")
--- 06 - GUI/Start.py
+++ 06 - GUI/Start.py
@@ -1,31 +1,63 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QDialog, QDialogButtonBox, QMainWindow
+# GUI-Test: Beispiel für eine grafische Benutzeroberfläche
+# (C) 22.04.2021, Michael Beurskens
+
+import sys   # Systemfunktionen: Erforderlich um Befehlszeilenparameter auszulesen und einen Rückgabewert zu liefern
+
+# Wichtige Funktionen für die grafische Benutzeroberfläche
+from PyQt5.QtWidgets import QApplication, QDialog, QDialogButtonBox
+
+# Selbst gestaltetete Oberfläche (generiert von QT Designer)
 from Ui_GUI import Ui_Dialog
 
 
+
+
+# Funktionen, die auf Änderungen in der Oberfläche reagieren
+
 def check():
-    if(ui.spinBox.value() > 0):
-        ui.pushButton.setEnabled(True) 
-    else:
-        ui.pushButton.setEnabled(False) 
+    """Prüft, ob die Eingabe gültig ist"""
+    if(ui.spinBox.value() > 0):              # Positiver Wert eingegeben
+        ui.pushButton.setEnabled(True)       # Knopf wird aktiviert
+    else:                                    # sonst (negativer Wert)
+        ui.pushButton.setEnabled(False)     # Knopf wird deaktiviert
 
 
 def update():
-    ui.textBrowser.insertPlainText(str(ui.spinBox.value())+"\n")
+    """Aktualisiert die Liste und setzt das Eingabefeld zurück"""
+    ui.textBrowser.insertPlainText(
+        str(ui.spinBox.value())+"\n")   # Neue Zeile in die Textanzeige einfügen
+    # Wert auf Null zurücksetzen
     ui.spinBox.setValue(0)
-    ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled((True))
+    ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(
+        (True))    # Ok-Knopf aktivieren
 
 
+# Das eigentliche Programm beginnt hier
+
+# Diese Zeile stellt sicher, dass die folgenden Befehle nur ausgeführt werden, wenn dieses Programm direkt gestartet (und nicht nur importiert) wird
 if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    w = QDialog()
-    ui = Ui_Dialog()
-    ui.setupUi(w)
-    ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled((False))
+
+    # Initialisiert die grafische Oberfläche und gibt ihr die zusätzlichen Argumente, die beim Start angegeben wurden
+    app: QApplication = QApplication(sys.argv)
+    w: QDialog = QDialog()   # Öffnet ein neues Fenster
+    # Generiert die Inhalte des Fensters anhand der Vorgaben, die mit QT Designer erstellt wurden
+    ui: Ui_Dialog = Ui_Dialog()
+    ui.setupUi(w)  # Fügt die einzelnen Elemente zum Fenster hinzu
+
+    # Funktionen des Programms
+    ui.buttonBox.button(QDialogButtonBox.Ok).setEnabled(
+        (False))  # OK-Knopf deaktivieren
+    # Bei Änderungen des Eingabefelds Knopf aktivieren oder deaktivieren
     ui.spinBox.valueChanged.connect(check)
+    # Bei Anklicken des Knopfes Inhalt des Eingabefelds an Liste anhängen und OK-Knopf aktivieren
     ui.pushButton.clicked.connect(update)
+
+    # Rückgabewert bei Schließen des Fensters durch Klick auf "OK" ausgeben
     w.accepted.connect(lambda: print("Ok"))
+    # Rückgabewert bei Schließen des Fensters durch Klick auf "Abbrechen" ausgeben
     w.rejected.connect(lambda: print("Abbruch"))
 
-    w.show()
+    w.show()  # Fenster anzeigen
+
+    # Beim Schließen des Fensters das Programm beenden und als Rückgabewert die Reaktion der grafischen Oberfläche weiterreichen
     sys.exit(app.exec_())
