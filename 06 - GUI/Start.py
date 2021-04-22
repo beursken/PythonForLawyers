48a7f861b3f81f2dfba045e41c2bdc5d234baa34
Michael Beurskens <beursken@uni-duesseldorf.de>
2021-04-22 09:12:37 +0200

    Example for User Interface Development using PyQt5 (early draft)

---
+++ 06 - GUI/Start.py
@@ -0,0 +1,11 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow,QDialog
+from Ui_GUI import Ui_Dialog
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = QDialog()
+    ui = Ui_Dialog()
+    ui.setupUi(w)
+    w.show()
+    sys.exit(app.exec_())
